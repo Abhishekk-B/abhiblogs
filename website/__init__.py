1edb630978195fead5a15674c5562e051e3d1e2b
Abhishek <95995839+Abhishekk-B@users.noreply.github.com>
2022-10-09 15:02:46 +0530

    Update __init__.py

--- website/__init__.py
+++ website/__init__.py
@@ -1,18 +1,17 @@
-from ensurepip import bootstrap
-import imp
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_ckeditor import CKEditor
 from flask_bootstrap import Bootstrap
+import os
 
 
 
 
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'MySecretKey'
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
+app.config['SECRET_KEY'] = 'dbfjshdbfjhMySecretKeyjdjjahbs'
+app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL", "sqlite:///database.db")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 ckeditor = CKEditor(app)
@@ -30,10 +29,3 @@ login_manager.init_app(app)
 @login_manager.user_loader
 def load_user(id):
     return User.query.get(int(id))
-
-
-
-
-
-
-
